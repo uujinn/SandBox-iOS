ae6d3851f84957b30b9ed9f0e4db10f9ee0b3b9f
Yujin Yang <70887135+uujinn@users.noreply.github.com>
2022-08-17 02:57:11 +0900

    [FEAT] Toss Clone

---
+++ Projects/KUHackathon/TossClone/TossClone/TossCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TossCloneApp.swift
+//  TossClone
+//
+//  Created by 양유진 on 2022/08/17.
+//
+
+import SwiftUI
+
+@main
+struct TossCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
